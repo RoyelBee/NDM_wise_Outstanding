a10f8bff2417e660af0cac6a6e037756e6c8bddc
Royel <royelsoft@gmail.com>
2020-05-06 15:37:52 +0600

    Matured credit aging

--- Functions/all_library.py
+++ Functions/all_library.py
@@ -2,11 +2,8 @@ import pandas as pd
 import pyodbc as db
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+import numpy as np
 
 
 
 
-conn = db.connect('DRIVER={SQL Server};'
-                        'SERVER=137.116.139.217;'
-                        'DATABASE=ARCHIVESKF;'
-                        'UID=sa;PWD=erp@123')
