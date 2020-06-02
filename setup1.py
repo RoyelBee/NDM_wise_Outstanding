3e448340adff8c1643ee0ca5519956297a8df6fd
Royel <royelsoft@gmail.com>
2020-06-02 13:14:18 +0600

    Marge all KPI

--- setup1.py
+++ setup1.py
@@ -1,51 +1,65 @@
 # #--------- 1. Total Outstanding ---------------------------------
-# out.totalOutstanding()
+import Functions.outstanding as out
+out.totalOutstanding()
 
 # # ------- 2. Credit Outstanding ---------------------------------
-# credit.creditOutstanding()
+import Functions.credit_outstanding as credit
+credit.creditOutstanding()
 
 # # ---. 3. NDM wise credit outstanding ---------------------------
-# ndm_out.ndm_wise_outstanding()
+import Functions.ndm_outstanding as ndm_out
+ndm_out.ndm_wise_outstanding()
 
 # # ---- 4. matured credit aging ----------------------------------
-# ndm_matured.matured_credit()
+import Functions.matured_credit_aging as ndm_matured
+ndm_matured.matured_credit()
 
 # # ----- 5. NDM matured credit aging -----------------------------
-# ndm_aging.ndm_matured_credit_aging()
+import Functions.ndm_matured_credit_aging as ndm_aging
+ndm_aging.ndm_matured_credit_aging()
 
 # -------- 6. Branch wise matured  credit aging  -------------------
 import Functions.branch_wise_matured_crdit as branch_matured
-# branch_matured.branch_wise_matured_credit()
+branch_matured.branch_wise_matured_credit()
 
 # # -- 7. Total Non-matured credit aging ---------------------------
 import Functions.total_non_matured_credit_aging as non_matured_credit
-# non_matured_credit.total_non_matured_credit_aging()
+non_matured_credit.total_non_matured_credit_aging()
 
 # # -- 8. NDM wise non-matured credit aging ------------------------
 import Functions.ndm_non_matured_credit_aging as ndm_non_matured
-# ndm_non_matured.ndm_non_matured_credit_aging()
+ndm_non_matured.ndm_non_matured_credit_aging()
 
 # # --- 9. Branch wise non-matured credit aging --------------------
 import Functions.branch_wise_non_matured_credit as branch_non_matured
-# branch_non_matured.branch_wise_non_matured_credit()
+branch_non_matured.branch_wise_non_matured_credit()
 
 # # ---10. Total Cash Drop Aging ----------------------------------
 import Functions.total_cashdrop_aging as cash_drop
-# cash_drop.cashdrop_aging()
+cash_drop.cashdrop_aging()
 
 # # ------------11. NDM wise cash drop aging ----------------------
 import Functions.NDM_wise_cash_drop_aging as ndm_cash_drop
-# ndm_cash_drop.ndm_cash_drop()
+ndm_cash_drop.ndm_cash_drop()
 
 # # -- 12. Branch wise cash drop aging ----------------
 import Functions.branch_wise_cash_drop_aging as branch_cash_drop
-# branch_cash_drop.branch_wise_cash_drop_aging()
+branch_cash_drop.branch_wise_cash_drop_aging()
 
 # # ------ 13. Nation wide return ---------------------------------
 import Functions.nation_wise_return as nation_return
-# nation_return.nation_wide_return()
+nation_return.nation_wide_return()
 
 # # -------14. Nation VS NDM Return ------------------------------
 import Functions.national_vs_ndm_return as nation_vs_ndm
 nation_vs_ndm.national_vs_ndm_return()
+
+# # -------15. Top 5 Branch Return -------------------------------
+import Functions.top5_branch_return as top5_branch
+top5_branch.top5_branch_return()
+
+# # ----  16.Top 5 Delivery Persons Return Amount ---------------
+import Functions.top5_delivery_person as delivery_persons_return
+delivery_persons_return.top5_delivery_persons_return()
+
 
